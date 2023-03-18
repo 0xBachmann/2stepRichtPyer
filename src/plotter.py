23d8524ef72232287e87fe7b9c628454f0faf260
Jonas Bachmann <jbachmann@ethz.ch>
2023-03-18 16:44:40 +0100

    plotter only init upon first plot

--- src/plotter.py
+++ src/plotter.py
@@ -4,7 +4,7 @@ import numpy as np
 from enum import Enum
 from copy import deepcopy
 from matplotlib.animation import FuncAnimation
-import subprocess
+import os
 from two_step_richtmeyer_util import Dimension
 
 
@@ -14,51 +14,65 @@ class plotting_Type(Enum):
 
 
 class Plotter:
-    def __init__(self, F: PDE, vals: np.ndarray, action, writeout, dim: Dimension, coords: list[np.ndarray, ...],
+    def __init__(self, F: PDE, action, writeout, dim: Dimension, coords: list[np.ndarray, ...],
                  filename=None):
-        self.ncomps = F.ncomp
+        self.ncomp = F.ncomp
         self.comp_names = F.comp_names
         self.dim = dim
-        self.fig, self.ax = plt.subplots(F.ncomp)
-        self.axs = [self.ax] if F.ncomp == 1 else self.ax
-
-        if self.dim >= Dimension.oneD:
-            self.x_coords = coords[0]
-        if self.dim >= Dimension.twoD:
-            self.y_coords = coords[1]
+        self.init = False
+        # Will be set the first time plotting happens
+        self.fig = None
+        self.ax = None
+        self.axs = None
+        self.ims = None
 
         if self.dim == Dimension.oneD:
             self.x_coords = coords[0]
-            self.ims = [self.axs[i].plot(self.x_coords, vals[..., i])[0] for i in range(self.ncomps)]
         elif self.dim == Dimension.twoD:
-            self.ims = [self.axs[i].imshow(vals[..., i]) for i in range(self.ncomps)]
+            pass
         else:
             raise NotImplementedError
 
         self.traj: list[tuple[np.ndarray, float], ...] = []
-
-        if self.dim >= Dimension.twoD:
-            for i in range(self.ncomps):
-                self.fig.colorbar(self.ims[i])
 
         assert action in ["show", "save"]
         self.action = action
         self.step = 0
         self.writeout = writeout
         self.PDE_type = f"{F.Type}"
-        self.filename = filename if filename is not None else "{self.PDE_type}.mp4"
-        self.time = 0
+        self.filename = filename if filename is not None else f"{self.PDE_type}.mp4"
+        self.time = 0.
+
+    def init_plots(self, vals: np.ndarray, time):
+        if self.init:
+            return
+
+        self.init = True
+        self.fig, self.ax = plt.subplots(self.ncomp)
+        self.axs = [self.ax] if self.ncomp == 1 else [ax for ax in self.ax]
+
+        if self.dim == Dimension.oneD:
+            self.ims = [self.axs[i].plot(self.x_coords, vals[..., i])[0] for i in range(self.ncomp)]
+        if self.dim == Dimension.twoD:
+            self.ims = [self.axs[i].imshow(vals[..., i]) for i in range(self.ncomp)]
+            for i in range(self.ncomp):
+                self.fig.colorbar(self.ims[i])
+
+        for i in range(self.ncomp):
+            self.axs[i].set_title(f"{self.comp_names[i]}\ntime: {time:.5}")
 
     def plot(self, vals: np.ndarray, time):
-        for i in range(self.ncomps):
-            self.axs[i].set_title(f"{self.comp_names[i]}\ntime: {time:.5}")
-            if self.dim == Dimension.oneD:
-                self.ims[i].set_data(self.x_coords, vals[..., i])
-            if self.dim == Dimension.twoD:
-                self.ims[i].set_data(vals[..., i])
+        if not self.init:
+            self.init_plots(vals, time)
+        else:
+            for i in range(self.ncomp):
+                self.axs[i].set_title(f"{self.comp_names[i]}\ntime: {time:.5}")
+                if self.dim == Dimension.oneD:
+                    self.ims[i].set_data(self.x_coords, vals[..., i])
+                if self.dim == Dimension.twoD:
+                    self.ims[i].set_data(vals[..., i])
 
     def write(self, vals: np.ndarray, dt):
-        self.time += dt
         if self.step % self.writeout == 0:
             if self.action == "show":
                 self.traj.append((deepcopy(vals), self.time))
@@ -66,15 +80,15 @@ class Plotter:
                 self.plot(vals, self.time)
                 plt.savefig(f"movie/{self.PDE_type}_{int(self.step / self.writeout)}.png")
 
+        self.time += dt
         self.step += 1
-
 
     def finlaize(self):
         if self.action == "show":
+            self.init_plots(*self.traj[0])
             ani = FuncAnimation(self.fig, lambda s: self.plot(*self.traj[s]), frames=int(self.step / self.writeout),
                                 blit=False)
             plt.show()
         elif self.action == "save":
-            subprocess.run(
-                ["ffmpeg", "-framerate", "30", "-i", f"'.'movie/{self.PDE_type}_%d.png'", f"movie/{self.filename}"])
-            subprocess.run(["rm", f"/home/jonas/bachelor-thesis/src/movie/{self.PDE_type}_*.png"])
+            os.system(f"ffmpeg -framerate 30 -i 'movie/{self.PDE_type}_%d.png' movie/{self.filename}")
+            os.system(f"rm movie/{self.PDE_type}_*.png")
