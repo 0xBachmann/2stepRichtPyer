2c3d8af0649c7176b9132affb730f88977a97dc8
Jonas Bachmann <jbachmann@ethz.ch>
2023-03-21 10:39:38 +0100

    add simple wave of density

--- src/richtmeyer2D.py
+++ src/richtmeyer2D.py
@@ -27,17 +27,26 @@ stepper = Richtmeyer2step(F, np.array([Lx, Ly]), np.array([200, 200]))
 # TODO: initial values
 def f(x):
     if Type == PDE_Type.Euler:
-        rho = 0.1
-        delta = 0.05
-        init = np.empty(F.ncomp)
-        init[0] = 1
-        if x[1] < Ly / 2:
-            init[1] = np.tanh(2 * np.pi / rho * (x[1] - 0.25))
+        if False:
+            rho = 0.1
+            delta = 0.05
+            init = np.empty(F.ncomp)
+            init[0] = 1
+            if x[1] < Ly / 2:
+                init[1] = np.tanh(2 * np.pi / rho * (x[1] - 0.25))
+            else:
+                init[1] = np.tanh(2 * np.pi / rho * (0.75 - x[1]))
+            init[2] = delta * np.sin(2 * np.pi * x[0])
+            init[3] = 1
+            return init
         else:
-            init[1] = np.tanh(2 * np.pi / rho * (0.75 - x[1]))
-        init[2] = delta * np.sin(2 * np.pi * x[0])
-        init[3] = 1
-        return init
+            init = np.empty(F.ncomp)
+            init[0] = 1 + 0.001 * np.sin(2 * np.pi / Lx * x[0]) * np.sin(2 * np.pi / Ly * x[1])
+            init[1] = 1
+            init[2] = 1
+            init[3] = 2
+            return init
+
     else:
         return np.exp(-np.linalg.norm((x - np.array([0.3, 0.3])))**2 / 0.01)
 
