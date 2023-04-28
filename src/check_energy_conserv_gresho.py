6b22c1e210cc20810a53383493471019fd0cf8ed
Jonas Bachmann <jbachmann@ethz.ch>
2023-04-28 20:14:22 +0200

    energy conservation

--- src/check_energy_conserv_gresho.py
+++ src/check_energy_conserv_gresho.py
@@ -17,11 +17,11 @@ F = Euler(5. / 3, dim=DIM)
 
 log("calculate initial conditions")
 
-resolution = np.array([40] * DIM.value)
+resolution = np.array([20] * DIM.value)
 Lx = 1
 Ly = Lx
 center = np.array([Lx / 2, Ly / 2])
-stepper_impl = Richtmeyer2stepImplicit(F, np.array([Lx, Ly]), resolution, eps=1e-9, method="krylov")
+stepper_impl = Richtmeyer2stepImplicit(F, np.array([Lx, Ly]), resolution, eps=1e-9, method="hybr", manual_jacobian=True)
 stepper = Richtmeyer2step(F, np.array([Lx, Ly]), resolution)
 
 M = 0.01
@@ -30,12 +30,21 @@ stepper.initial_cond(lambda x: gresho_vortex(x, center, F, Mmax=M, qr=0.4 * np.p
 stepper_impl.initial_cond(lambda x: gresho_vortex(x, center, F, Mmax=M, qr=0.4 * np.pi * Lx / 1))
 
 
+def E(v, Ekin=False, Eint=False):
+    if Ekin:
+        return np.sum(0.5 * np.sum(v[..., 1:DIM.value + 1] ** 2, axis=-1) / v[..., 0])
+    elif Eint:
+        return np.sum(v[..., -1] - 0.5 * np.sum(v[..., 1:DIM.value + 1] ** 2, axis=-1) / v[..., 0])
+    else:
+        return np.sum(v[..., -1])
+
+
 energies = []
-energies_impl = []
 times = []
 
-energies.append(np.sum(stepper.grid_no_ghost[..., -1]))
-energies_impl.append(np.sum(stepper_impl.grid_no_ghost[..., -1]))
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+energies.append([E(stepper.grid_no_ghost), E(stepper.grid_no_ghost, Ekin=True)])
 times.append(0)
 
 T = t
@@ -44,17 +53,46 @@ while time < T:
     dt = stepper.cfl()
     stepper.step(dt)
 
-    print(f"dt = {dt}, time = {time:.3f}/{T}")
+    print(f"dt = {dt}, time = {time:.3f}/{T}", end="\r")
     time += dt
 
-    energies.append(np.sum(stepper.grid_no_ghost[..., -1]))
-    energies_impl.append(np.sum(stepper_impl.grid_no_ghost[..., -1]))
+    energies.append([E(stepper.grid_no_ghost), E(stepper.grid_no_ghost, Ekin=True)])
     times.append(time)
 
-plt.plot(times, energies, label="explicit")
-plt.plot(times, energies_impl, label="implicit")
+
+energies = np.array(energies)
+ax1.plot(times, energies[:, 0], label="explicit")
+ax2.plot(times, energies[:, 1], label="explicit")
+
+for i in [1, 2, 4, 8, 10, 15, 20, 30, 50, 100]:
+    print(f"\n{i}")
+    stepper_impl.initial_cond(lambda x: gresho_vortex(x, center, F, Mmax=M, qr=0.4 * np.pi * Lx / 1))
+
+    energies = []
+    times = []
+
+    energies.append([E(stepper.grid_no_ghost), E(stepper.grid_no_ghost, Ekin=True)])
+    times.append(0)
+
+    time = 0.
+    while time < T:
+        dt = stepper.cfl() * i
+        stepper_impl.step(dt)
+
+        print(f"dt = {dt}, time = {time:.3f}/{T}", end="\r")
+        time += dt
+
+        energies.append([E(stepper.grid_no_ghost), E(stepper.grid_no_ghost, Ekin=True)])
+        times.append(time)
+
+    energies = np.array(energies)
+    ax1.plot(times, energies[:, 0], label=f"implicit, {i}*cfl")
+    ax2.plot(times, energies[:, 1], label=f"implicit, {i}*cfl")
+
 plt.xlabel("time")
 plt.ylabel("Energy")
-plt.legend()
-plt.savefig(Path("ims", "energy_conserve_gresho.png"))
-
+ax1.set_title("Total Energy")
+ax2.set_title("Kinetic Energy")
+fig.legend()
+plt.tight_layout()
+plt.savefig(Path("ims", "energy_gresho_hybr.png"), dpi=200)
